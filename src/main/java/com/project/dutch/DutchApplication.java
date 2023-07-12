3f17de97b205ab0dbb01bbdb5be284d471273fd3
aditya dubey <48158220+dubeyaditya29@users.noreply.github.com>
2023-07-12 09:23:03 +0530

    Initial commit

---
+++ src/main/java/com/project/dutch/DutchApplication.java
@@ -0,0 +1,13 @@
+package com.project.dutch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dutch/DutchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dutch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
